80f07ff0d9ae32b45e2202ab4797deee56de106b
Tim Scott <99059109+TheTimbob@users.noreply.github.com>
2025-01-04 00:24:05 -0500

    Implement about me page from document in storage.

---
+++ cmd/web/about.go
@@ -0,0 +1,45 @@
+package web
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"path"
+	"timterests/internal/models"
+	"timterests/internal/storage"
+)
+
+func AboutHandler(w http.ResponseWriter, r *http.Request, storageInstance models.Storage) {
+	var about models.About
+
+	// Get all articles from the storage
+	prefix := "about/"
+	aboutFile, err := storage.ListObjects(context.Background(), storageInstance, prefix)
+	if err != nil {
+		http.Error(w, "Failed to fetch about info", http.StatusInternalServerError)
+		return
+	}
+
+	key := *aboutFile[0].Key
+	fileName := path.Base(key)
+	localFilePath := path.Join("s3", fileName)
+
+	document, err := storage.ReadFile(key, localFilePath, storageInstance)
+	if err != nil {
+		http.Error(w, "Failed to read about file", http.StatusInternalServerError)
+		return
+	}
+
+	about = models.About{
+		Title:    document.Title,
+		Subtitle: document.Subtitle,
+		Body:     document.Body,
+	}
+
+	component := AboutForm(about)
+	err = component.Render(r.Context(), w)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		log.Fatalf("Error rendering in GetArticleByIDHandler: %e", err)
+	}
+}
